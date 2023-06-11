0d44af777362360e0989d9042be3c740fca424b8
shiva-murugan <130650884+shiva-murugan@users.noreply.github.com>
2023-06-11 10:27:50 +0530

    Add files via upload

---
+++ questionsbank.js
@@ -0,0 +1,28 @@
+export const Questions=[
+    {
+        questions:"1) what is answer 1+3?",
+        options:["24","21","4","6"],
+        answer:"4",
+    },
+    {
+        questions:"2) what is the color banana?",
+        options:["Orange","Yellow","Blue","White"],
+        answer:"Yellow", 
+    },
+    {
+        questions:"3) what is the color apples?",
+        options:["Red","Yellow","Blue","White"],
+        answer:"Red"
+    },
+    {
+        questions:"4) what is  answer 45-9?",
+        options:["2","15","14","36"],
+        answer:"36",
+    },
+    {
+        questions:"5) what is the name of our Prime Minister?",
+        options:["Modi","Putin","Sankar","Biden"],
+        answer:"Modi",
+    },
+    
+];
